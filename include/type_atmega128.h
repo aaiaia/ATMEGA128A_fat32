03b6700cd8203f7c4c97e04066b44455fe2bbb21
PleYa <aaiaia.gg@gmail.com>
2022-03-19 02:06:39 +0900

    detached type_atmega128

--- include/type_atmega128.h
+++ include/type_atmega128.h
@@ -2,4 +2,12 @@
 #define _TYPE_ATMEGA128_H_
 
 
+#define SIGNED_8BITS	(signed char)
+#define SIGNED_16BITS	(signed int)
+#define SIGNED_32BITS	(signed long)
+
+#define UNSIGNED_8BITS	(unsigned char)
+#define UNSIGNED_16BITS	(unsigned int)
+#define UNSIGNED_32BITS	(unsigned long)
+
 #endif
