efbcbc446c8afc210e1aa9eb2f6bcf811828dbd5
PleYa <aaiaia.gg@gmail.com>
2022-03-19 02:46:52 +0900

    detached to include/keypad.h

--- include/keypad.h
+++ include/keypad.h
@@ -1,5 +1,19 @@
 #ifndef _KEYPAD_H_
 #define _KEYPAD_H_
 
+#define KEY_OUT_LEFT			1
+#define KEY_OUT_LEFT_PORT		PORTA
+
+#define KEY_OUT_CENTER			7
+#define KEY_OUT_CENTER_PORT		PORTD
+
+#define KEY_OUT_RIGHT			6
+#define KEY_OUT_RIGHT_PORT		PORTD
+
+#define KEY_IN_TOP				5
+#define KEY_IN_TOP_PORT			PIND
+
+#define KEY_IN_BOTTOM			4
+#define KEY_IN_BOTTON_PORT		PIND
 
 #endif
