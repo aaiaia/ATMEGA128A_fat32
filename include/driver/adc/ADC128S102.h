81b6e11d72132e58f6feb5800148dab950d51596
PleYa <aaiaia.gg@gmail.com>
2022-03-28 01:03:22 +0900

    added function declare to header

--- include/driver/adc/ADC128S102.h
+++ include/driver/adc/ADC128S102.h
@@ -5,4 +5,7 @@
 
 #define ADC_DEFAULT_JUNK_SAMPLING_TIMES		0x10
 
+unsigned char SPI_ADC_Init();
+unsigned int getAdcValue(unsigned char spiDeviceNumber, unsigned char numberOfPort, unsigned int samplingTimes, unsigned char junkTimes);
+
 #endif
