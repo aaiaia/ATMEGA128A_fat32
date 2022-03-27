7b3d8a20cdc5a6edb928ea02a13da2c9107df3f5
PleYa <aaiaia.gg@gmail.com>
2022-03-28 01:00:14 +0900

    detached external adc(ADC128S102) to driver file

--- include/driver/adc/ADC128S102.h
+++ include/driver/adc/ADC128S102.h
@@ -1,3 +1,8 @@
 #ifndef _ADC128S102_H_
 #define _ADC128S102_H_
+
+#define GET_ADC_PORT_INSTRUCTION(adcAddr)		(adcAddr<<3)//that is instruction of ADC128S102.
+
+#define ADC_DEFAULT_JUNK_SAMPLING_TIMES		0x10
+
 #endif
--- main.c
+++ main.c
@@ -49,64 +49,6 @@ unsigned char testFlag = 0;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////// ADC128S //////////////////////////////////
-#define GET_ADC_PORT_INSTRUCTION(adcAddr)		(adcAddr<<3)//that is instruction of ADC128S102.
-
-#define ADC_DEFAULT_JUNK_SAMPLING_TIMES		0x10
-
-unsigned char SPI_ADC_Init()
-{
-	SPI_ADC_SPCR_SET;
-	SPI_ADC_SPSR_SET;
-	return 0;
-}
-/*
-	description of getAdcValue
-	.
-	.
-	.
-*/
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-example Codes
-#define ADC_DEFAULT_JUNK_SAMPLING_TIMES
-getAdcValue(SPI_MODE_ADC0, 0, 10, ADC_DEFAULT_JUNK_SAMPLING_TIMES)
-*/
-unsigned int getAdcValue(unsigned char spiDeviceNumber, unsigned char numberOfPort, unsigned int samplingTimes, unsigned char junkTimes)
-{
-		unsigned int SPI_result = 0;
-		unsigned long int avrResult = 0;
-		unsigned int samplingTimesTemp = samplingTimes;
-
-		spiDeviceHold(spiDeviceNumber);
-		
-		for(junkTimes=junkTimes;junkTimes!=0; junkTimes--)
-		{
-		//ADC dummy Communication, that is two times.
-			spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-			spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-
-			spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-			spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-		}
-
-		for(samplingTimes=samplingTimes; samplingTimes!=0; samplingTimes--)
-		{
-		//ADC dummy Communication, that is two times.
-			spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-			spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-
-			SPI_result = spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-			SPI_result = (SPI_result<<8);
-			SPI_result |= spiTransCeive(GET_ADC_PORT_INSTRUCTION(numberOfPort));
-
-			//processing of ADC data
-			avrResult+=SPI_result;
-		}
-		spiDeviceRelease();
-
-		return ((unsigned int)(avrResult/samplingTimesTemp));
-}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
