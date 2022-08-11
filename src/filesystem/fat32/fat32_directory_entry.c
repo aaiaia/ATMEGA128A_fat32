436f34024bd42a553174a9171e5f11eb5c9274af
PleYa <aaiaia.gg@gmail.com>
2022-08-11 17:46:42 +0900

    moved timeinfo abstraction functions to from fat32_time.c to fat32_directory_entry.c

--- src/filesystem/fat32/fat32_directory_entry.c
+++ src/filesystem/fat32/fat32_directory_entry.c
@@ -63,3 +63,84 @@ char abstractDirLongNameFromDirectoryEntry(char *dirEntry, char *longName)//if l
 
 	return 0;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void dirDateInfoParseFromDirectoryEntry(char *entry, unsigned char offset, dirDateInfo *date)
+{
+	if((offset == DIR_CREATION_DATE_OFFSET)||(offset == DIR_LAST_ACCESS_DATE_OFFSET)||(offset == DIR_WRITE_DATE_OFFSET))
+	{
+		unsigned int temp = abstractLittleEndianTo16Bits(entry+offset);
+		(*date).year=((temp&DIR_DATE_YEAR_BIT_MASK)>>DIR_DATE_YEAR_BIT_OFFSET);
+		(*date).month=((temp&DIR_DATE_MONTH_BIT_MASK)>>DIR_DATE_MONTH_BIT_OFFSET);
+		(*date).day=((temp&DIR_DATE_DAY_BIT_MASK)>>DIR_DATE_DAY_BIT_OFFSET);
+	}
+	else
+	{
+		(*date).year=2000;
+		(*date).month=1;
+		(*date).day=1;
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void dirTimeInfoParseFromDirectoryEntry(char *entry, unsigned char offset, dirTimeInfo *time)
+{
+	if((offset == DIR_CREATION_TIME_OFFSET)||(offset == DIR_WRITE_TIME_OFFSET))
+	{
+		unsigned int temp = abstractLittleEndianTo16Bits(entry+offset);
+		(*time).hour=((temp&DIR_TIME_HOUR_BIT_MASK)>>DIR_TIME_HOUR_BIT_OFFSET);
+		(*time).minute=((temp&DIR_TIME_MINUTE_BIT_MASK)>>DIR_TIME_MINUTE_BIT_OFFSET);
+		(*time).second=((temp&DIR_TIME_SECOND_BIT_MASK)>>DIR_TIME_SECOND_BIT_OFFSET);
+	}
+	else
+	{
+		(*time).hour=0;
+		(*time).minute=0;
+		(*time).second=0;
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void dirDateAndTimeInfoParseFromDirectoryEntry(char *entry, directoryStructure *p)
+{
+	dirDateInfoParseFromDirectoryEntry(entry, DIR_CREATION_DATE_OFFSET, &((*p).createDateInfo.date));
+	dirDateInfoParseFromDirectoryEntry(entry, DIR_LAST_ACCESS_DATE_OFFSET, &((*p).writeDateInfo.date));
+	dirDateInfoParseFromDirectoryEntry(entry, DIR_WRITE_DATE_OFFSET, &((*p).lastestAccessDate));
+
+	dirTimeInfoParseFromDirectoryEntry(entry, DIR_CREATION_TIME_OFFSET, &((*p).createDateInfo.time));
+	dirTimeInfoParseFromDirectoryEntry(entry, DIR_WRITE_TIME_OFFSET, &((*p).writeDateInfo.time));
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+unsigned int dirDateInfoTransformTo16Bits(dirDateInfo *date)
+{
+	return ((((unsigned int)(*date).year)-DIR_DATE_YEAR_START)<<DIR_DATE_YEAR_BIT_OFFSET)|(((unsigned int)(*date).month)<<DIR_DATE_MONTH_BIT_OFFSET)|(((unsigned int)(*date).day)<<DIR_DATE_DAY_BIT_OFFSET);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+unsigned int dirTimeInfoTransformTo16Bits(dirTimeInfo *time)
+{
+	return (((unsigned int)(*time).hour)<<DIR_TIME_HOUR_BIT_OFFSET)|(((unsigned int)(*time).minute)<<DIR_TIME_MINUTE_BIT_OFFSET)|(((unsigned int)(*time).second)<<DIR_TIME_SECOND_BIT_OFFSET);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void dateInfoConvertToDirectoryEntry(char *entry, unsigned offset, dirDateInfo *date)
+{
+	switch(offset)
+	{
+			case DIR_CREATION_DATE_OFFSET:
+			case DIR_LAST_ACCESS_DATE_OFFSET:
+			case DIR_WRITE_DATE_OFFSET:
+				parsing16BitsToLittleEndian(entry+offset, dirDateInfoTransformTo16Bits(date));
+				break;
+			default:
+				parsing16BitsToLittleEndian(entry+offset, 0x3C21);
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void timeInfoConvertToDirectoryEntry(char *entry, unsigned offset, dirTimeInfo *time)
+{
+	switch(offset)
+	{
+		case DIR_CREATION_TIME_OFFSET:
+		case DIR_WRITE_TIME_OFFSET:
+			parsing16BitsToLittleEndian(entry+offset, dirTimeInfoTransformTo16Bits(time));
+			break;
+		default:
+			parsing16BitsToLittleEndian(entry+offset, 0);
+	}
+}
